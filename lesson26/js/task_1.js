d5c3c9d0845b7f68f7fb151cfb5a285933205879
Savkiv <ivan.savkiv.igorovych>
2021-05-30 10:56:16 +0300

    Done first task

--- lesson26/js/task_1.js
+++ lesson26/js/task_1.js
@@ -7,3 +7,23 @@
 // Функция для подсчета необходимого времени для преодоления
 // переданного расстояния со средней скоростью. Учтите,
 // что через каждые 4 часа дороги водителю необходимо делать перерыв на 1 час.
+const car = {
+  manufacturer: "Russia Tolyatti",
+  model: 21074,
+  year: 2008,
+  speed: 100,
+  informationToScreen: function toScreen() {
+    return `The manufacturer is ${this.manufacturer}, model - ${this.model}, year of manufacture is ${this.year} and average speed is ${this.speed} km/h`;
+  },
+  time: function timeSpend(length) {
+    const message =
+      "The time needed to overcome transmitted distance at medium speed is:";
+    const time = Math.floor(length / car.speed);
+    const rest = Math.floor(time / 4);
+    if (time % 4 === 0) {
+      return `${message} ${time + rest - 1} hours`;
+    } else {
+      return `${message} ${time + rest} hours`;
+    }
+  },
+};
